99e7c57f9cafd032144770a233f11954974d0062
nikelborm <evadev@duck.com>
2025-04-07 06:35:55 +0300

    quick save: Mon Apr  7 06:35:55 MSK 2025

---
+++ lib.spec.ts
@@ -0,0 +1,152 @@
+import { color } from "bun" with { type: "json" };
+import { expect, test } from "bun:test";
+import {
+  countBigintLeadingZeros,
+  countBigintUsedBits,
+  getBigintSlotFromLeft,
+  getBigintSlotFromRight,
+  withUpdatedSlotCountingFromRight,
+} from "lib.js";
+
+const renderTestNum = (e: any, pad?: { binary?: number; decimal?: number }) =>
+  typeof e === "bigint" || typeof e === "number"
+    ? ((ending) =>
+        color("rgb(209, 167, 0)", "ansi") +
+        e.toString(10).padStart(pad?.decimal ?? 1, " ") +
+        color("rgb(251, 227, 133)", "ansi") +
+        ending +
+        color("gray", "ansi") +
+        ` \\* 0b` +
+        e.toString(2).padStart(pad?.binary ?? 1, "0") +
+        ending +
+        ` *\\` +
+        color("white", "ansi"))(typeof e === "bigint" ? "n" : "")
+    : e;
+
+const testSuite = <TArgs extends Array<any>, TReturn>(
+  func: (...args: TArgs) => TReturn
+) => {
+  return <const TestCasesArray extends Array<[expected: TReturn, ...TArgs]>>(
+    ...testCases: TestCasesArray
+  ) => {
+    if (!testCases.length)
+      throw new Error("testSuite should be provided with test cases");
+
+    const formattingArr = Array.from<
+      | {
+          binary: number;
+          decimal: number;
+        }
+      | undefined
+    >({
+      length: Math.max(...testCases.map((_) => _.length)),
+    });
+
+    for (
+      let testCaseParamIndex = 0;
+      testCaseParamIndex < testCases[0]!.length;
+      testCaseParamIndex++
+    ) {
+      const isThisParamANumber = testCases
+        .map((testCase) => typeof testCase[testCaseParamIndex])
+        .every((_) => _ === "number" || _ === "bigint");
+
+      if (!isThisParamANumber) continue;
+
+      formattingArr[testCaseParamIndex] = {
+        binary: Math.max(
+          ...testCases.map(
+            (testCase) =>
+              (testCase[testCaseParamIndex] as number | bigint).toString(2)
+                .length
+          )
+        ),
+        decimal: Math.max(
+          ...testCases.map(
+            (testCase) =>
+              (testCase[testCaseParamIndex] as number | bigint).toString(10)
+                .length
+          )
+        ),
+      };
+    }
+
+    testCases.forEach(([expected, ...args], testCaseIndex) =>
+      test(
+        color("rgb(0, 140, 153)", "ansi") +
+          func.name +
+          color("white", "ansi") +
+          `(` +
+          args
+            .map((e, index) => renderTestNum(e, formattingArr[1 + index]))
+            .join(color("white", "ansi") + ", ") +
+          color("white", "ansi") +
+          `)` +
+          color("rgb(82, 178, 238)", "ansi") +
+          ` === ` +
+          renderTestNum(expected, formattingArr[0]) +
+          (testCaseIndex + 1 === testCases.length ? "\n" : ""),
+        () => expect(func(...args)).toBe(expected)
+      )
+    );
+  };
+};
+
+// prettier-ignore
+testSuite(countBigintLeadingZeros)(
+  /* expected x            powerOf2ToGetAssumedBigIntSize                             */
+  [  0n,      0b1n,        0n /* 2 ** 0n === 1n (represents 1 bit of length)       */ ],
+  [  7n,      0b00000001n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
+  [  5n,      0b00000101n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
+);
+
+// prettier-ignore
+testSuite(countBigintUsedBits)(
+  /* expected x         powerOf2ToGetAssumedBigIntSize                             */
+  [  1n,      0b1n,     0n /* 2 ** 0n === 1n (represents 1 bit of length)       */ ],
+  [  1n,      0b00001n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
+  [  3n,      0b00101n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
+);
+
+// prettier-ignore
+testSuite(getBigintSlotFromRight)(
+  /* expected x               slotIndex slotSizeInBits */
+  [  0b1n,    0b1n,           0n,       1n             ],
+  [  0b1n,    0b1_0n,         1n,       1n             ],
+  [  0b01n,   0b01_10n,       1n,       2n             ],
+  [  0b01n,   0b01_10n,       1n,       2n             ],
+  [  0b101n,  0b101_000_110n, 2n,       3n             ],
+  [  0b000n,  0b101_000_110n, 1n,       3n             ],
+  [  0b110n,  0b101_000_110n, 0n,       3n             ],
+);
+
+// prettier-ignore
+testSuite(getBigintSlotFromLeft)(
+  /* expected x                     slotIndex slotSizeInBits assumedBigintSizeInBits */
+  [  0b1n,    0b1n,                 0n,       1n,            1n                      ],
+
+  [  0b1n,    0b0_0_0_0_0_0_0_0_1n, 8n,       1n,            9n                      ],
+  [  0b1n,    0b0_0_0_0_0_0_0_1_0n, 7n,       1n,            9n                      ],
+
+  [  0b01n,   0b00_00_00_01_10n,    3n,       2n,            10n                     ],
+  [  0b01n,   0b00_00_00_01_10n,    3n,       2n,            10n                     ],
+
+  [  0b101n,  0b101_000_110n,       0n,       3n,            9n                      ],
+  [  0b000n,  0b101_000_110n,       1n,       3n,            9n                      ],
+  [  0b110n,  0b101_000_110n,       2n,       3n,            9n                      ],
+);
+
+// prettier-ignore
+testSuite(withUpdatedSlotCountingFromRight)(
+  /* expected             x                   slotIndexCountingFromRight highestValueInSlot newSlotContent */
+  [  0b0n,                0b0n,               0n,                        0b1n,              0b0n           ],
+  [  0b1n,                0b0n,               0n,                        0b1n,              0b1n           ],
+  [  0b0n,                0b1n,               0n,                        0b1n,              0b0n           ],
+  [  0b1n,                0b1n,               0n,                        0b1n,              0b1n           ],
+  [  0b101_010_101_000n,  0b101_010_101_010n, 0n,                        0b111n,            0b000n         ],
+  [  0b101_010_101_111n,  0b101_010_101_010n, 0n,                        0b111n,            0b111n         ],
+  [  0b101_010_000_010n,  0b101_010_101_010n, 1n,                        0b111n,            0b000n         ],
+  [  0b101_010_111_010n,  0b101_010_101_010n, 1n,                        0b111n,            0b111n         ],
+  [  0b101_000_101_010n,  0b101_010_101_010n, 2n,                        0b111n,            0b000n         ],
+  [  0b101_111_101_010n,  0b101_010_101_010n, 2n,                        0b111n,            0b111n         ],
+);
--- lib.ts
+++ lib.ts
@@ -1,92 +1,5 @@
-import { color } from "bun" with { type: "json" };
-import { expect, test } from "bun:test";
-
 // can be disable for performance
 const enableSafeGuards = true;
-
-const renderTestNum = (e: any, pad?: { binary?: number; decimal?: number }) =>
-  typeof e === "bigint" || typeof e === "number"
-    ? ((ending) =>
-        color("rgb(209, 167, 0)", "ansi") +
-        e.toString(10).padStart(pad?.decimal ?? 1, " ") +
-        color("rgb(251, 227, 133)", "ansi") +
-        ending +
-        color("gray", "ansi") +
-        ` \\* 0b` +
-        e.toString(2).padStart(pad?.binary ?? 1, "0") +
-        ending +
-        ` *\\` +
-        color("white", "ansi"))(typeof e === "bigint" ? "n" : "")
-    : e;
-
-const testSuite = <TArgs extends Array<any>, TReturn>(
-  func: (...args: TArgs) => TReturn
-) => {
-  return <const TestCasesArray extends Array<[expected: TReturn, ...TArgs]>>(
-    ...testCases: TestCasesArray
-  ) => {
-    if (!testCases.length)
-      throw new Error("testSuite should be provided with test cases");
-
-    const formattingArr = Array.from<
-      | {
-          binary: number;
-          decimal: number;
-        }
-      | undefined
-    >({
-      length: Math.max(...testCases.map((_) => _.length)),
-    });
-
-    for (
-      let testCaseParamIndex = 0;
-      testCaseParamIndex < testCases[0]!.length;
-      testCaseParamIndex++
-    ) {
-      const isThisParamANumber = testCases
-        .map((testCase) => typeof testCase[testCaseParamIndex])
-        .every((_) => _ === "number" || _ === "bigint");
-
-      if (!isThisParamANumber) continue;
-
-      formattingArr[testCaseParamIndex] = {
-        binary: Math.max(
-          ...testCases.map(
-            (testCase) =>
-              (testCase[testCaseParamIndex] as number | bigint).toString(2)
-                .length
-          )
-        ),
-        decimal: Math.max(
-          ...testCases.map(
-            (testCase) =>
-              (testCase[testCaseParamIndex] as number | bigint).toString(10)
-                .length
-          )
-        ),
-      };
-    }
-
-    testCases.forEach(([expected, ...args], testCaseIndex) =>
-      test(
-        color("rgb(0, 140, 153)", "ansi") +
-          func.name +
-          color("white", "ansi") +
-          `(` +
-          args
-            .map((e, index) => renderTestNum(e, formattingArr[1 + index]))
-            .join(color("white", "ansi") + ", ") +
-          color("white", "ansi") +
-          `)` +
-          color("rgb(82, 178, 238)", "ansi") +
-          ` === ` +
-          renderTestNum(expected, formattingArr[0]) +
-          (testCaseIndex + 1 === testCases.length ? "\n" : ""),
-        () => expect(func(...args)).toBe(expected)
-      )
-    );
-  };
-};
 
 // TODO: check will it instead of manual bringing it to 8n work stable with
 // inexact power of 2, like when assumedBigintSizeInBits === 5n
@@ -129,14 +42,6 @@ export const countBigintLeadingZeros = (
   return n - (y !== 0n ? 0b00010n : bitSequence);
 };
 
-// prettier-ignore
-testSuite(countBigintLeadingZeros)(
-  /* expected x            powerOf2ToGetAssumedBigIntSize                             */
-  [  0n,      0b1n,        0n /* 2 ** 0n === 1n (represents 1 bit of length)       */ ],
-  [  7n,      0b00000001n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
-  [  5n,      0b00000101n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
-);
-
 export const countBigintUsedBits = (
   bitSequence: bigint,
   /* Required to be power of 2 because this function uses binary search to count leading zeros */
@@ -148,15 +53,7 @@ export const countBigintUsedBits = (
   );
 };
 
-// prettier-ignore
-testSuite(countBigintUsedBits)(
-  /* expected x         powerOf2ToGetAssumedBigIntSize                             */
-  [  1n,      0b1n,     0n /* 2 ** 0n === 1n (represents 1 bit of length)       */ ],
-  [  1n,      0b00001n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
-  [  3n,      0b00101n, 3n /* 2 ** 3n === 8n (the closest highest above 5 bits) */ ],
-);
-
-const getBigintSlotFromRight = (
+export const getBigintSlotFromRight = (
   bitSequence: bigint,
   slotIndexCountingFromRight: bigint,
   slotSizeInBits: bigint
@@ -174,18 +71,6 @@ const getBigintSlotFromRight = (
     ((1n << slotSizeInBits) - 1n)
   );
 };
-
-// prettier-ignore
-testSuite(getBigintSlotFromRight)(
-  /* expected x               slotIndex slotSizeInBits */
-  [  0b1n,    0b1n,           0n,       1n             ],
-  [  0b1n,    0b1_0n,         1n,       1n             ],
-  [  0b01n,   0b01_10n,       1n,       2n             ],
-  [  0b01n,   0b01_10n,       1n,       2n             ],
-  [  0b101n,  0b101_000_110n, 2n,       3n             ],
-  [  0b000n,  0b101_000_110n, 1n,       3n             ],
-  [  0b110n,  0b101_000_110n, 0n,       3n             ],
-);
 
 export const getBigintSlotFromLeft = (
   bitSequence: bigint,
@@ -215,23 +100,12 @@ export const getBigintSlotFromLeft = (
   );
 };
 
-// prettier-ignore
-testSuite(getBigintSlotFromLeft)(
-  /* expected x                     slotIndex slotSizeInBits assumedBigintSizeInBits */
-  [  0b1n,    0b1n,                 0n,       1n,            1n                      ],
-
-  [  0b1n,    0b0_0_0_0_0_0_0_0_1n, 8n,       1n,            9n                      ],
-  [  0b1n,    0b0_0_0_0_0_0_0_1_0n, 7n,       1n,            9n                      ],
-
-  [  0b01n,   0b00_00_00_01_10n,    3n,       2n,            10n                     ],
-  [  0b01n,   0b00_00_00_01_10n,    3n,       2n,            10n                     ],
-
-  [  0b101n,  0b101_000_110n,       0n,       3n,            9n                      ],
-  [  0b000n,  0b101_000_110n,       1n,       3n,            9n                      ],
-  [  0b110n,  0b101_000_110n,       2n,       3n,            9n                      ],
-);
-
 export function* genBigints(amountOfSlots: bigint, highestValueInSlot: bigint) {
+  if (enableSafeGuards) {
+    if (amountOfSlots < 1n)
+      throw new Error("Amount of slots should be greater than 0!");
+  }
+
   const slotSizeInBits = countBigintUsedBits(highestValueInSlot);
 
   function* genBigintsInner(
@@ -275,18 +149,3 @@ export const withUpdatedSlotCountingFromRight = (
     (newSlotContent << (slotIndexCountingFromRight * slotSizeInBits))
   );
 };
-
-// prettier-ignore
-testSuite(withUpdatedSlotCountingFromRight)(
-  /* expected             x                   slotIndexCountingFromRight highestValueInSlot newSlotContent */
-  [  0b0n,                0b0n,               0n,                        0b1n,              0b0n           ],
-  [  0b1n,                0b0n,               0n,                        0b1n,              0b1n           ],
-  [  0b0n,                0b1n,               0n,                        0b1n,              0b0n           ],
-  [  0b1n,                0b1n,               0n,                        0b1n,              0b1n           ],
-  [  0b101_010_101_000n,  0b101_010_101_010n, 0n,                        0b111n,            0b000n         ],
-  [  0b101_010_101_111n,  0b101_010_101_010n, 0n,                        0b111n,            0b111n         ],
-  [  0b101_010_000_010n,  0b101_010_101_010n, 1n,                        0b111n,            0b000n         ],
-  [  0b101_010_111_010n,  0b101_010_101_010n, 1n,                        0b111n,            0b111n         ],
-  [  0b101_000_101_010n,  0b101_010_101_010n, 2n,                        0b111n,            0b000n         ],
-  [  0b101_111_101_010n,  0b101_010_101_010n, 2n,                        0b111n,            0b111n         ],
-);
